7ff846c2bb49a050d2dd9b81311813fe1037270b
Saksham Pardesi <saksham@visionary.org.in>
2026-03-16 13:34:36 +0530

    firecrawl ai integrated

--- src/app/api/demo/backgound/route.ts
+++ src/app/api/demo/backgound/route.ts
@@ -1,8 +1,9 @@
 import { inngest } from "@/inngest/client";
-export async function POST(){
+export async function POST(req: Request){
+    const { prompt } = await req.json();
     await inngest.send({
     name: "demo/generate",
-    data:{}
+    data:{ prompt }
     })
   return Response.json({status:"started"});
 }
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,29 +1,42 @@
 import { generateText } from "ai";
 import { inngest } from "./client";
 import { google } from "@ai-sdk/google";
+import { firecrawl } from "@/lib/firecrawl";
+
+
+const URL_REGEX = /(https?:\/\/[^\s]+)/g;
+
 
 export const demoGenerate = inngest.createFunction(
   { id: "demo-generate" },
   { event: "demo/generate" },
   async ({ event, step }) => {
+    const {prompt} = event.data as {prompt:string};
+
+    const urls = await step.run("extract-urls",async ()=>{
+        return prompt.match(URL_REGEX) || [];
+    }) as string[];
+
+    const scrapeContent = await step.run("scrape_urls",async ()=>{
+        const results = await Promise.all(urls.map(async (url)=>{
+          const result =  await firecrawl.scrape(url,{
+            formats: ['markdown']
+          })
+          return result.markdown ?? null;
+        }))
+        return results.filter(Boolean).join("\n\n")
+    })
+
+    const finalPrompt = scrapeContent ? `Content:\n${scrapeContent}\n\nQuestion:\n${prompt}` : prompt;
+
+
     const result = await step.run("generate-text", async () => {
       const response = await generateText({
         model: google("gemini-2.5-flash"),
-        prompt: "Explain how black holes are formed in 1000 sentences.",
+        prompt: finalPrompt,
       });
       return response.text;
-    });
 
-    await step.sleep("wait-2-mins", "2m");
-
-    const summary = await step.run("summarize-text", async () => {
-      const response = await generateText({
-        model: google("gemini-2.5-flash"),
-        prompt: `Summarize this in one sentence: ${result}`,
-      });
-      return response.text;
-    });
-
-    return { result, summary };
+    })
   },
 );
